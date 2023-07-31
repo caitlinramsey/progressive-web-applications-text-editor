866b9ad9a524aadadcbcf57e74995367214a8528
Caitlin <caitlinramsey@outlook.com>
2023-07-30 22:01:48 -0400

    added logic to src-sw.js

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,10 +1,11 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidated } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
+// precacheAndRoute() takes an array of URLs to precache. The self._WB_Manifest is an array that contains the list of URLs to precache
 precacheAndRoute(self.__WB_MANIFEST);
 
 const pageCache = new CacheFirst({
@@ -26,5 +27,21 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+// Set up asset cache
+registerRoute(
+  // Define the callback function that will filter the requests to be cached
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidated({
+    // Name of cache storage
+    cacheName: 'asset-cache',
+     plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      })
+     ],
+  })
+);
